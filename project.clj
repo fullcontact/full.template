e877faa5dcdb987de16f9137bad59eae0e6b3e7d
Karlis Lauva <k@karlis.me>
2016-06-27 14:17:02 -0600

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject full-template/lein-template "0.2.1-SNAPSHOT"
+(defproject full-template/lein-template "0.2.1"
   :description "Template for a full.monty HTTP project"
   :url "http://github.com/fullcontact/full.template"
   :license {:name "Eclipse Public License"
