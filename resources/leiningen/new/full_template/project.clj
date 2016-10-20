1455ff3a3abc007f677f0a08d57c45773d95cf25
Karlis Lauva <k@karlis.me>
2016-10-20 10:45:51 -0600

    update dependencies

--- resources/leiningen/new/full_template/project.clj
+++ resources/leiningen/new/full_template/project.clj
@@ -1,11 +1,10 @@
 (defproject fullcontact/{{ name }} "latest"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [fullcontact/full.bootstrap "0.10.2"]]
+                 [fullcontact/full.bootstrap "0.10.3"]]
   :main {{ name }}.api
   :resource-paths ["resources"]
   :target-path "target/%s"
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:uberjar {:aot [{{ name }}.api]}
              :dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.7.0"]
-                                  [midje-junit-formatter "0.1.0-SNAPSHOT"]]}})
+                   :dependencies [[midje "1.8.3"]]}})
