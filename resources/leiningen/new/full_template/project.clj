eb4061996a2710198857e777540b39270b443994
Karlis Lauva <k@karlis.me>
2017-09-21 11:34:19 +0300

    bump deps

--- resources/leiningen/new/full_template/project.clj
+++ resources/leiningen/new/full_template/project.clj
@@ -1,6 +1,12 @@
 (defproject fullcontact/{{ name }} "latest"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [fullcontact/full.bootstrap "0.10.3"]]
+                 [fullcontact/full.core "0.10.1" :exclusions [org.clojure/clojurescript]]
+                 [fullcontact/camelsnake "0.9.0"]
+                 [fullcontact/full.json "0.10.1"]
+                 [fullcontact/full.metrics "0.11.4" :exclusions [fullcontact/full.async]]
+                 [fullcontact/full.cache "1.0.1" :exclusions [fullcontact/full.async]]
+                 [fullcontact/full.http "1.0.1" :exclusions [fullcontact/full.async]]
+                 [fullcontact/full.async "1.0.0" :exclusions [org.clojure/clojurescript]]]
   :main {{ name }}.api
   :resource-paths ["resources"]
   :target-path "target/%s"
